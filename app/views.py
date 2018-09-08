8f2c4735d87cdc9fab2e8f572a0b488ee9655474
xu <1356452615@qq.com>
2018-09-08 18:20:40 +0800

    add learn.html . about_me.html

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # Create your views here.
 from django.urls import reverse
-from backweb.models import Article
+from backweb.models import Article, AType
 from rest_framework import mixins,viewsets
 from app.app_serializers import ArticleSerializer
 
@@ -23,18 +24,16 @@ class ArticleSource(mixins.ListModelMixin, #查询所有学生
 
 def index(request):
     if request.method=="GET":
-        all_article=Article.objects.filter(is_read=True).order_by('-id')
-
-
+        article=Article.objects.filter(is_read=True).order_by('-id')
+        now_page = int(request.GET.get('page', 1))
+        paginator = Paginator(article,1)
+        all_article = paginator.page(now_page)
         return render(request,'web/index.html',{'all_article':all_article})
 
 def about_me(request):
     if request.method=="GET":
         return render(request,'web/about.html')
 
-def slow_life(request):
-    if request.method=="GET":
-        return render(request,'web/slow_life.html')
 
 def mumble(request):
     if request.method=="GET":
@@ -42,7 +41,23 @@ def mumble(request):
 
 def dont_stop_learn(request):
     if request.method=="GET":
-        return render(request,'web/learn.html')
+        type =int(request.GET.get('type',0))
+        if type==0:
+            type_article=Article.objects.filter( atype =1 or 2 or 3 or 4, is_read=True).order_by('-id')
+        elif type== 1:
+            type_article = Article.objects.filter(atype=1,is_read=True).order_by('-id')
+        elif type== 2:
+            type_article = Article.objects.filter(atype=2,is_read=True).order_by('-id')
+        elif type == 3:
+            type_article = Article.objects.filter(atype=3,is_read=True).order_by('-id')
+        elif type == 4:
+            type_article = Article.objects.filter(atype=4,is_read=True).order_by('-id')
+
+        now_page = int(request.GET.get('page',1))
+        paginator = Paginator(type_article,5)
+        page_art = paginator.page(now_page)
+
+        return render(request,'web/learn.html',{'page_art':page_art,'type':type})
 
 def content(request):
     if request.method=="GET":
@@ -50,3 +65,12 @@ def content(request):
         article=Article.objects.get(id=id)
         return render(request,'web/content.html',{'article':article})
 
+
+def life(request):
+    if request.method=="GET":
+        life_art=Article.objects.filter(atype=5,is_read=True)
+        now_page = int(request.GET.get('page', 1))
+        paginator = Paginator(life_art,5)
+        page_art = paginator.page(now_page)
+
+        return render(request,'web/slow_life.html',{'page_art':page_art})
